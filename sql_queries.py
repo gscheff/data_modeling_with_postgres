0c54112c142dfd3d00a49e0ca56c5832e4b6202d
Gerhard Scheffler <gerhard.scheffler@gmail.com>
2020-04-19 17:24:52 +0200

    feat: add foreign key constrains

--- sql_queries.py
+++ sql_queries.py
@@ -11,11 +11,11 @@ time_table_drop = "DROP TABLE IF EXISTS time;"
 songplay_table_create = ("""
 CREATE TABLE IF NOT EXISTS songplays (
   songplay_id SERIAL,
-  start_time BIGINT,
-  user_id VARCHAR,
+  start_time BIGINT REFERENCES time(start_time),
+  user_id VARCHAR REFERENCES users(user_id),
   level VARCHAR,
-  song_id VARCHAR,
-  artist_id VARCHAR,
+  song_id VARCHAR REFERENCES songs(song_id),
+  artist_id VARCHAR REFERENCES artists(artist_id),
   session_id VARCHAR,
   location VARCHAR,
   user_agent VARCHAR,
@@ -116,11 +116,11 @@ WHERE songs.title = %s
 # QUERY LISTS
 
 create_table_queries = [
-    songplay_table_create,
     user_table_create,
     song_table_create,
     artist_table_create,
     time_table_create,
+    songplay_table_create,
 ]
 
 drop_table_queries = [
